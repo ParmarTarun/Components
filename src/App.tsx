e386fcd206cad90bf9cd2c4ae19d0a70a2b23b85
parmartarun <parmartarun.dev@gmail.com>
2024-04-01 16:07:45 -0400

    ⚡️ Add Dummy Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { components } from "./components/Index";
-import { FaArrowCircleRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaRegCircleDot } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
 
 const App = () => {
   const [componentIndex, setComponentIndex] = useState(0);
@@ -8,34 +10,47 @@ const App = () => {
     <div className="app">
       <div className="components-wrapper">{components[componentIndex]}</div>
 
-      {componentIndex !== 0 && <hr className="border border-primary w-4/5" />}
+      <hr className="border border-primary w-4/5" />
 
       <div className="components-action">
-        {componentIndex === 0 && componentIndex < components.length - 1 ? (
-          <div className="m-auto">
-            <button onClick={() => setComponentIndex(1)}>
-              Checkout
-              <FaArrowCircleRight className="inline ml-2" />
-            </button>
+        <div className="components-navigation">
+          <button
+            onClick={() => setComponentIndex((prev) => prev - 1)}
+            disabled={componentIndex === 0}
+            className="disabled:cursor-not-allowed"
+          >
+            <FaChevronLeft />
+          </button>
+
+          <div className="progress">
+            {components.map((_, i) => {
+              if (i === componentIndex) {
+                return (
+                  <FaRegCircleDot
+                    key={i}
+                    className="cursor-pointer"
+                    onClick={() => setComponentIndex(i)}
+                  />
+                );
+              }
+              return (
+                <GoDotFill
+                  key={i}
+                  className="cursor-pointer"
+                  onClick={() => setComponentIndex(i)}
+                />
+              );
+            })}
           </div>
-        ) : (
-          <div className="components-navigation">
-            {componentIndex > 0 ? (
-              <button onClick={() => setComponentIndex((prev) => prev - 1)}>
-                Back
-              </button>
-            ) : (
-              <p></p>
-            )}
-            {componentIndex < components.length - 1 ? (
-              <button onClick={() => setComponentIndex((prev) => prev + 1)}>
-                Next
-              </button>
-            ) : (
-              <p></p>
-            )}
-          </div>
-        )}
+
+          <button
+            onClick={() => setComponentIndex((prev) => prev + 1)}
+            disabled={componentIndex >= components.length - 1}
+            className="disabled:cursor-not-allowed"
+          >
+            <FaChevronRight />
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/DummyComponent/Index.tsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+// add the component to src/components/index.tsx file to render
+const DummyComponent = () => {
+  return <div className=""></div>;
+};
+
+export default DummyComponent;
